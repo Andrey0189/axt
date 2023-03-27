9fba34fc0a056baea929998565b374287ef7137e
Andrew <39239738+Andrey0189@users.noreply.github.com>
2023-03-27 23:38:19 +0300

    Update config.js
    obed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	TOKEN: 'MTA4OTA3MTcwMDMzMjc5MzkwNg.GqZmZS.wqnLoegjQP29SV7pqoBVaxwNwoNg7ITnmYGkMo',
+	TOKEN: 'token',
 	ID: '1089071700332793906',
 	whitelist: ['242975403512168449']
 }
