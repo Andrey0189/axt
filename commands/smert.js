61e4a66cb268ec91f34477caf7f639ff881359e8
Andrey0189 <varnavsky06@gmail.com>
2023-04-07 18:32:28 +0300

    Smert module

--- commands/smert.js
+++ commands/smert.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
 	name: 'smert',
 	description: 'Send смерть emoji',
 	run: (intr) => { intr.reply('https://media.discordapp.net/attachments/809366426224820255/902651201710088222/atoms-cry.gif') }
