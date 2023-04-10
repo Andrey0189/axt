c0fca7c4c0e0ac5e22dd7fb1afc608d81135313b
Andrey0189 <varnavsky06@gmail.com>
2023-04-10 17:15:31 +0300

    wtf

--- commands/finger.js
+++ commands/finger.js
@@ -1,5 +1,7 @@
 export default {
 	name: 'finger',
 	description: 'FINGER! I LAID FINGER ON HIM',
-	run: (intr) => { intr.reply('Железный палец Майка Эрмантраута') }
+	run: (intr) => {
+		intr.reply('Железный палец Майка Эрмантраута')
+	}
 }
