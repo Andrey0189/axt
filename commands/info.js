5fd3d83b1bfe3e204e689a106e4ca09b098a6073
Andrey0189 <varnavsky06@gmail.com>
2023-04-10 14:28:26 +0300

    bug fixes

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,9 @@ export default {
 	name: 'info',
 	description: 'Prints bot info',
 	run: async (intr) => {
+		const hostLatency = Date.now() - intr.createdTimestamp
 		await intr.reply('Loading...')
+
 		const child_process = await import('child_process')
 		const amperfetch = child_process.execSync('./etc/sysfetch.sh')
 
@@ -12,9 +14,18 @@ export default {
 		botinfo += `Commands: **\`${Bot.commands.length}\`**`
 
 		let nodeinfo = ''
-		nodeinfo += `NodeJS version: **\`${process.version}\`**\n`
+		nodeinfo += `Version: **\`${process.version}\`**\n`
 		nodeinfo += `RAM usage: **\`${(process.memoryUsage().rss / 1024 / 1024).toFixed(1)} MB\`**\n`
 		nodeinfo += `Uptime: **\`${Bot.ms(process.uptime() * 1000)}\`**`
+
+		const packagejson = await Bot.fs.readFileSync('./package.json')
+		const obj = JSON.parse(packagejson)
+		const djsversion = obj.dependencies['discord.js'].replace('^', 'v')
+
+		let discordjsinfo = ''
+		discordjsinfo += `Version: **\`${djsversion}\`**\n`
+		discordjsinfo += `API latency: **\`${Bot.client.ws.ping} ms\`**\n`
+		discordjsinfo += `Host latency: **\`${hostLatency} ms\`**`
 
 
 		const embed = new Bot.Discord.EmbedBuilder()
@@ -22,6 +33,7 @@ export default {
 			.setColor('#2071c7')
 			.addFields({ name: `Bot info:`, value: botinfo, inline: true })
 			.addFields({ name: `NodeJS info:`, value: nodeinfo, inline: true })
+			.addFields({ name: `Discord.js info:`, value: discordjsinfo, inline: true })
 			.addFields({ name: 'System information:', value: '```markdown\n' + amperfetch + '\n```' })
 
 		intr.editReply({ content: '', embeds: [embed] })
